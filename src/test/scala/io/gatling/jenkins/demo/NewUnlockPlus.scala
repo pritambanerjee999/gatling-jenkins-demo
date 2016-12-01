bae011679050dab3c6c9fa0702e7c6d672079ab5
PRITAM BANERJEE <pritambanerjee999@gmail.com>
2016-11-30 16:05:42 -0800

    Update NewUnlockPlus.scala

--- src/test/scala/io/gatling/jenkins/demo/NewUnlockPlus.scala
+++ src/test/scala/io/gatling/jenkins/demo/NewUnlockPlus.scala
@@ -12,16 +12,6 @@ class NewUnlockPlus extends Simulation {
   var startEpoch = 0L;
   var stopEpoch = 0L;
 
-  val appId = Map("appId" -> "NIKEPLUSGPS")
-
-  //Sync service parameters
-  val syncUrl = "https://sync.plus.nikecloud.com"
-
-  val syncAuth = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzY3AiOiJ2My1zeW5jLXBheWxvYWQiLCJleHAiOjc0MDk4NDczMDUsInN1YiI6InYzLWxzcyIsImF1ZCI6WyJ2My1zeW5jLXNlcnZpY2UiXSwiaXNzIjoidjMtbHNzIn0.jOGoWH5JVXiDYAgcjXv3B6lWs4BT-Hd_-tno7emY90Q"
-
-  var httpConf = http.baseURL(syncUrl)
-
-
   def startGenerator() :Long = {
     var startNum = 1396024675000L + ThreadLocalRandom.current().nextLong(90000L)
     startEpoch = startNum;
@@ -37,12 +27,6 @@ class NewUnlockPlus extends Simulation {
   val postActivity = scenario("POST Activity")
     .exec(http("POST activity post")
       .post("/activity/")
-      .header("Content-Type", "application/json; charset=ISO-8859-1")
-      .header("USERID", "14986456963")
-      .header("appid" , "NIKEPLUSGPS")
-      .header("accept" , "*/*")
-      .header("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJ1YXR0ZXN0YXBwIiwic2NwIjpbInJlYWQiLCJ3cml0ZSJdLCJzdWIiOiJ1YXR0ZXN0YXBwIiwiYXVkIjpbInYzLXN5bmMtc2VydmljZSJdLCJleHAiOjE3ODY4NzUxMjF9.VvAJIo0TfjWa2F0WeoneR3drApB8eRhHVK41_0KnQ4f9KSvVTAOeBBSzGs0tvoujAEkE5sv-eaBt7efIJ5Ao23vw8A2n7Dtgi4njJcWD6SJpLArgPfLUGd0XHdvaN-yI1Ap5PSWrhlELKXHNSPJudfv9PQ1pJrpd5lBpHFZOTD6NLt3A6neWuV2H-9zv6P2wzERMJ2HbuE-YPnppnZYRYMGWM8SSnUV-C6IMg1ZbkDG0TSkx4vMy3s_qB3fDTkEfqM-6MnQwPBnb6cCoBJLcorxxpnjNoW1vpdctGkmIS08iNhOhDMi-vt4cP3MvAYrgUQI2lD08iQ3vTVntT4-5cg")
-      .header("X-Nike-AppId", "uattestapp")
       .body(StringBody(session =>
         s"""
            |{
